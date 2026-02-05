07443deeeef427d441968aa480b3ae4f22f1a4cd
Shaline Wambui <shalinewambui04@gmail.com>
2026-02-05 11:54:13 +0300

    Added routes for easier navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import { HomePage } from './utils/homepage'
+import { Game } from './utils/game'
+import HomePage from './utils/homepage'
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<HomePage/>} />
+      <Route path="/game/:levelId" element={<Game/>}/>
     </Routes>
   )
 }
+
 
 export default App
